173f9b430bb055ae3f95692cf90ac13cacda2de3
ChanduKern <chandu@kernsoftwaresolutions.com>
2026-02-10 19:35:47 +0530

    Create Book.java

---
+++ LibraryManagementSystem/Book.java
@@ -0,0 +1,38 @@
+public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean issued;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.issued = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return issued;
+    }
+
+    public void issueBook() {
+        issued = true;
+    }
+
+    public void returnBook() {
+        issued = false;
+    }
+}
