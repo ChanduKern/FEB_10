bbd4d1df6e2c0bb8ecdac4f94a8066410f22744e
ChanduKern <chandu@kernsoftwaresolutions.com>
2026-02-10 19:37:22 +0530

    Add Member class for library management system

---
+++ LibraryManagementSystem/Member.java
@@ -0,0 +1,14 @@
+public class Member {
+
+    private int memberId;
+    private String name;
+
+    public Member(int memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
