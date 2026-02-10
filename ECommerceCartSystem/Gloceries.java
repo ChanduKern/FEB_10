35fe12ced0ea146bf6076e5bc3c4e889ac85a4fc
ChanduKern <chandu@kernsoftwaresolutions.com>
2026-02-10 16:28:07 +0530

    Create Gloceries.java

---
+++ ECommerceCartSystem/Gloceries.java
@@ -0,0 +1,17 @@
+public class Groceries implements Product {
+    private String name;
+    private double price;
+
+    public Groceries(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
